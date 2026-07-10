5f284bc780cc514d697ca07110a5b35556a43996
shiva praneeth <shivarajapeta@gmail.com>
2026-07-10 20:07:45 +0530

    Add varia.js

---
+++ varia.js
@@ -0,0 +1,30 @@
+const accountId=1434333
+let accountEmail = "shivarajapeta@gmail.com"
+var accountPassword="12345"
+accountCity="hyd"
+
+// accountId=23
+console.log(accountId);
+console.table([accountId,accountEmail, accountCity, accountPassword])
+ 
+"use strict"; // treat all the js code as newer version
+// alert(3+3) this does not work in the vs code because we are using node js not browser 
+
+
+let name ="shiva" 
+let age=18  // 2 ^53 paduthai
+//bigint  kuda vadutharu for bigger values .for ex in trading apps !
+let isLoggedIn=true // boolean datatype
+// null =>standalone value
+// undefined => value not assigned 
+// symbol 
+
+
+
+// object  this is veryyy imppporrtanttt \
+console.log(typeof name); // string
+console.log(typeof age); // number
+//******************************************** */
+console.log(typeof null); // object  ************************************************
+console.log(typeof undefined); // undefined****
+
